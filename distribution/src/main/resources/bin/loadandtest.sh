655cc63c517e9bff950f0c36a31c1b3040c515f8
xiafan68 <xiafan68@gmail.com>
2016-01-27 19:51:11 +0800

    echo verbose of the command

--- distribution/src/main/resources/bin/loadandtest.sh
+++ distribution/src/main/resources/bin/loadandtest.sh
@@ -13,15 +13,23 @@ dataDir="/home/xiafan/data/twitter/sortedsegs"
 t_lsmi="conf/twitter/scale/index_lsmi_twitter.conf"
 t_lsmo="conf/twitter/scale/index_lsmo_twitter.conf"
 idx=4
-for partDir in ${dataDir}:
+while [ $idx -le 20 ] 
 do
+    dataFile=${dataDir}/part${idx}
     sleep 10
-    bin/indexloader.sh -l conf/log4j-server2.properties -c ${t_lsmi} -d $dataFile
+    echo "bin/indexloader.sh -l conf/log4j-server2.properties -c ${t_lsmi} -d $dataFile"
+    #bin/indexloader.sh -l conf/log4j-server2.properties -c ${t_lsmi} -d $dataFile
+
     sleep 10
-    bin/perftest -e ${odir}/part${idx} -c ${t_lsmi} -q ${tqueryseed} -s single
+    echo "bin/perftest -e ${odir}/part${idx} -c ${t_lsmi} -q ${tqueryseed} -s single"
+    #bin/perftest -e ${odir}/part${idx} -c ${t_lsmi} -q ${tqueryseed} -s single
+    
     sleep 10
-    bin/indexloader.sh -l conf/log4j-server2.properties -c ${t_lsmo} -d $dataFile
+    echo " bin/indexloader.sh -l conf/log4j-server2.properties -c ${t_lsmo} -d $dataFile"
+    #bin/indexloader.sh -l conf/log4j-server2.properties -c ${t_lsmo} -d $dataFile
+    
     sleep 10
-    bin/perftest -e ${odir}/part${idx} -c ${t_lsmo} -q ${tqueryseed} -s single
+    echo "bin/perftest -e ${odir}/part${idx} -c ${t_lsmo} -q ${tqueryseed} -s single"
+    #bin/perftest -e ${odir}/part${idx} -c ${t_lsmo} -q ${tqueryseed} -s single
     idx=`expr ${idx} + 4`
 done
