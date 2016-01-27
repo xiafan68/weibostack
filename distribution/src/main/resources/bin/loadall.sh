848313380e88190ab75bce75625786f081bb9786
xiafan <xiafan68@gmail.com>
2016-01-27 19:52:55 +0800

    only load weibo

--- distribution/src/main/resources/bin/loadall.sh
+++ distribution/src/main/resources/bin/loadall.sh
@@ -2,17 +2,18 @@
 
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTPATH=$(dirname $(dirname "$SCRIPT"))
 echo $SCRIPTPATH
 
 dataFile="/home/xiafan/data/weibo/sortedsegs"
 for conf in ${SCRIPTPATH}/conf/weibo/*
 do
-    bin/indexloader.sh -l conf/log4j-server2.properties -c $conf -d $dataFile
+    echo "loading file ${conf}"
+    bin/indexloader -l conf/log4j-server2.properties -c $conf -d $dataFile
 done
 
 dataFile="/home/xiafan/data/twitter/sortedsegs"
 for conf in ${SCRIPTPATH}/conf/twitter/*
 do
-    bin/indexloader.sh -l conf/log4j-server2.properties -c $conf -d $dataFile
+    bin/indexloader -l conf/log4j-server2.properties -c $conf -d $dataFile
 done
