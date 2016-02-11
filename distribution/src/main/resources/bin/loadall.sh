0c8a5eb4a4a2c2480a7d634b33bb76934f2e27ae
xiafan <xiafan68@gmail.com>
2016-02-11 23:35:03 +0800

    add test configuration files and script

--- distribution/src/main/resources/bin/loadall.sh
+++ distribution/src/main/resources/bin/loadall.sh
@@ -8,20 +8,16 @@ echo $SCRIPTPATH
 dataFile="/home/xiafan/data/weibo/part20"
 for conf in ${SCRIPTPATH}/conf/weibo/ttest/*
 do
-<<<<<<< HEAD
     echo "loading file ${conf}"
-    bin/indexloader -l conf/log4j-server2.properties -c $conf -d $dataFile
-=======
-    bin/indexloader.sh -l conf/log4j-server2.properties -c $conf -d $dataFile -e ~/expr/weibo_throughput -s $1
->>>>>>> c0539989dcd6de93f79681f2eac0d8038a7a3153
+    #bin/indexloader -l conf/log4j-server2.properties -c $conf -d $dataFile
+    fname=$(basename $conf)
+    bin/throughputtest -c $conf -d $dataFile -e ~/expr/weibo_throughput/$fname -s $1
 done
 
 dataFile="/home/xiafan/data/twitter/part20"
 for conf in ${SCRIPTPATH}/conf/twitter/ttest/*
 do
-<<<<<<< HEAD
-    bin/indexloader -l conf/log4j-server2.properties -c $conf -d $dataFile
-=======
-    bin/indexloader.sh -l conf/log4j-server2.properties -c $conf -d $dataFile -e ~/expr/twitter_throughput -s $2
->>>>>>> c0539989dcd6de93f79681f2eac0d8038a7a3153
+    fname=$(basename $conf)
+    #bin/indexloader -l conf/log4j-server2.properties -c $conf -d $dataFile
+    bin/throughputtest -c $conf -d $dataFile -e ~/expr/twitter_throughput/$fname -s $2
 done
